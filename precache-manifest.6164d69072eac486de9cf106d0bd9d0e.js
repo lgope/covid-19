f6884baa975d50db9137252e467baefdfd232b03
lgope <ilakshmangope@gmail.com>
2020-04-07 22:20:59 +0600

    Updates

---
+++ precache-manifest.6164d69072eac486de9cf106d0bd9d0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55f9ab364d5e9197bde7e05593d78de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1943dd26b289548391ab",
+    "url": "/static/css/main.63c815a6.chunk.css"
+  },
+  {
+    "revision": "3d2f32cb312a8604a47e",
+    "url": "/static/js/2.fd5a33fa.chunk.js"
+  },
+  {
+    "revision": "ee4ee1ec0345baf3ccdd39c5f31204f6",
+    "url": "/static/js/2.fd5a33fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1943dd26b289548391ab",
+    "url": "/static/js/main.46ce8626.chunk.js"
+  },
+  {
+    "revision": "94a45316d015e52eacc6",
+    "url": "/static/js/runtime-main.60cbd1db.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
